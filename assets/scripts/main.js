1cd4ed9a8efbc59a6a097d635491b4669cd5453a
semenov-vldr <semenov-vldr@users.noreply.github.com>
2024-06-16 09:33:55 +0000

    deploy: 0cb11a882aaca8c44c6f0bf45d1882ac3fac408a

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -462,3 +462,44 @@ function hideLoader() {
 }
 ;
 window.addEventListener('load', hideLoader);
+"use strict";
+
+var reviewsSlider = document.querySelector(".reviews__slider");
+if (reviewsSlider) {
+  var mySwiper = new Swiper(reviewsSlider, {
+    // pagination: {
+    //   el: '.swiper-pagination',
+    //   clickable: true,
+    // },
+    // navigation: {
+    //   nextEl: '.slider-nav__next',
+    //   prevEl: '.slider-nav__prev',
+    // },
+
+    grabCursor: true,
+    slidesPerView: 3,
+    loop: true,
+    effect: "coverflow",
+    centeredSlides: true,
+    initialSlide: 1,
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 160,
+      depth: 30,
+      modifier: 4.5,
+      slideShadows: false
+    },
+    // Ширина экрана
+    breakpoints: {
+      // 320: {
+      //   slidesPerView: 1.2,
+      //   spaceBetween: 16,
+      // },
+
+      // 768: {
+      //   slidesPerView: 1.5,
+      //   spaceBetween: 24,
+      // },
+    }
+  });
+}
